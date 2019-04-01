cc9d3e0ba5c98139929b34df869f474329f2ca10
anees mangi <mangianees@gmail.com>
2019-04-01 18:22:46 +0100

    complete

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Templets</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+
+</head>
+<body>
+    
+
+<nav class="navbar navbar-expand-lg navbar-light" id="tnav">
+   
+    <form class="form-inline my-2 my-lg-0">    
+    
+        <div class="nbtn">
+      <button class="btn btn-outline-dark " type="submit">Home</button>
+      </div>
+      <div class="nbtn">
+      <button class="btn btn-outline-dark " type="submit">About</button>
+</div>
+      <div class="nbtn">
+      <button class="btn btn-outline-dark " type="submit">Product</button>
+</div>
+      <div class="nbtn">
+      <button class="btn btn-outline-dark " type="submit">Services</button>
+</div>
+
+</div>
+      <div class="nbtn">
+<div class="dropdown">
+  <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  More
+  </button>
+  <div class="dropdown-menu" id="ddnav">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+    
+  </div>
+</div>
+</div>
+    </form>
+
+    
+</nav>
+
+
+
+
+
+
+
+
+</body>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
